00ad3a73db032b45a0a0f0a0f6c489c343356228
Shiv Vyas <shivvyas0209@gmail.com>
2025-02-08 23:18:00 -0500

    chore(Global) Safe Area Context added

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet } from 'react-native';
 
 // Import screens from your components folder
 import OnboardingScreen1 from './components/screens/onboarding/OnboardingScreen1';
@@ -8,16 +10,30 @@ import OnboardingScreen3 from './components/screens/onboarding/OnboardingScreen3
 import LoginScreen from './components/screens/auth/LoginScreen';
 import Dashboard from './components/screens/Dashboard';
 import TabNavigator from './navigation/TabNavigator';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        {/* <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top', 'bottom']}>
+        <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            {/* <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+});
+
